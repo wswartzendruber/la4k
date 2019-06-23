c4fd9190114cfc0bf7786672c6f5cf060be3cd98
William Swartzendruber <wswartzendruber@gmail.com>
2019-06-23 14:08:44 -0600

    Testing structure in place.

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,16 +23,34 @@ kotlin {
                 implementation(kotlin("stdlib-common"))
             }
         }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+                implementation("org.la4k:la4k-test-metadata:0.0.1")
+            }
+        }
 
         js().compilations["main"].defaultSourceSet {
             dependencies {
                 implementation(kotlin("stdlib-js"))
             }
         }
+        js().compilations["test"].defaultSourceSet {
+            dependencies {
+                implementation("org.la4k:la4k-test-js:0.0.1")
+            }
+        }
 
         jvm().compilations["main"].defaultSourceSet {
             dependencies {
                 implementation(kotlin("stdlib-jdk8"))
+            }
+        }
+        jvm().compilations["test"].defaultSourceSet {
+            dependencies {
+                implementation(kotlin("test-junit"))
+                implementation("org.la4k:la4k-test-jvm:0.0.1")
             }
         }
     }
---
+++ src/commonTest/kotlin/Tests.kt
@@ -0,0 +1,7 @@
+package org.la4k.api
+
+import kotlin.test.assertTrue
+import kotlin.test.Test
+
+class Tests {
+}
