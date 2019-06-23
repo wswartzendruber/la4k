1769e895abe291736d68671efe5ebdcbb08572dd
William Swartzendruber <wswartzendruber@gmail.com>
2019-06-23 14:38:23 -0600

    Fixing default throwable as null in Logger.

--- src/commonMain/kotlin/Logger.kt
+++ src/commonMain/kotlin/Logger.kt
@@ -4,111 +4,111 @@ public class Logger(val name: String) {
 
     public fun off(
         message: CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = log(name, Level.off, message, throwable, tags)
 
     public fun off(
         block: () -> CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = logConditional(name, Level.off, block, throwable, tags)
 
     public fun fatal(
         message: CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = log(name, Level.fatal, message, throwable, tags)
 
     public fun fatal(
         block: () -> CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = logConditional(name, Level.fatal, block, throwable, tags)
 
     public fun error(
         message: CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = log(name, Level.error, message, throwable, tags)
 
     public fun error(
         block: () -> CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = logConditional(name, Level.error, block, throwable, tags)
 
     public fun warn(
         message: CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = log(name, Level.warn, message, throwable, tags)
 
     public fun warn(
         block: () -> CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = logConditional(name, Level.warn, block, throwable, tags)
 
     public fun info(
         message: CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = log(name, Level.info, message, throwable, tags)
 
     public fun info(
         block: () -> CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = logConditional(name, Level.info, block, throwable, tags)
 
     public fun debug(
         message: CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = log(name, Level.debug, message, throwable, tags)
 
     public fun debug(
         block: () -> CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = logConditional(name, Level.debug, block, throwable, tags)
 
     public fun trace(
         message: CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = log(name, Level.trace, message, throwable, tags)
 
     public fun trace(
         block: () -> CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = logConditional(name, Level.trace, block, throwable, tags)
 
     public fun all(
         message: CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = log(name, Level.all, message, throwable, tags)
 
     public fun all(
         block: () -> CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = logConditional(name, Level.all, block, throwable, tags)
 
     public fun custom(
         level: Level,
         message: CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = log(name, level, message, throwable, tags)
 
     public fun custom(
         level: Level,
         block: () -> CharSequence,
-        throwable: Throwable?,
+        throwable: Throwable? = null,
         tags: List<String>? = null
     ) = logConditional(name, level, block, throwable, tags)
 
