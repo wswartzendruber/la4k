21c54f780d387c8d0ccdef8f63e9a391a06804f6
William Swartzendruber <wswartzendruber@gmail.com>
2020-05-31 15:37:01 -0600

    Correcting indentation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@
  */
 
  plugins {
-     kotlin("multiplatform").version("1.3.61").apply(false)
-     kotlin("jvm").version("1.3.61").apply(false)
+    kotlin("multiplatform").version("1.3.61").apply(false)
+    kotlin("jvm").version("1.3.61").apply(false)
  }
 
  allprojects {
