2579a523ae9d241b8550489598c733f59dec6112
William Swartzendruber <wswartzendruber@gmail.com>
2020-08-15 12:48:47 -0600

    Putting build scripts and test code in the public domain.

--- la4k-test/src/commonTest/kotlin/TestTests.kt
+++ la4k-test/src/commonTest/kotlin/TestTests.kt
@@ -1,9 +1,6 @@
 /*
- * Copyright 2020 William Swartzendruber
- *
- * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a
- * copy of the MPL was not distributed with this file, You can obtain one at
- * https://mozilla.org/MPL/2.0/.
+ * Any copyright is dedicated to the Public Domain.
+ * https://creativecommons.org/publicdomain/zero/1.0/
  */
 
 package org.la4k.test.test
